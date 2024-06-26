bbba0177a39ea5654ad121d276f0b64ae9ec845a
Cactus PC <richardcao789@gmail.com>
2024-06-26 13:51:31 -0700

    Initial commit

---
+++ TextClassificationDataset.py
@@ -0,0 +1,41 @@
+import json
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+#Create a Text-Classification Dataset that parses our JSON file
+class TCD(Dataset):
+
+    #Creates private variable questions and labels, for a random list of train data
+    def __init__(self, json_path, training_set=True):
+        questions = []
+        labels = []
+        map = {0:[], 1:[], 2:[]}
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+            for type in data["assignmentType"]:
+                for q in type['questions']:
+                    questions.append(q)
+                    #using int to represent label for PyTorch to do work
+                    labels.append(int(type['tag']))
+                for r in type['responses']:
+                    map[int(type['tag'])].append(r)
+        
+        total_questions = len(questions)
+        #using numpy to choose random questions and labels to do our training
+        train_indices = np.random.choice(total_questions, int(0.8 *total_questions), replace=False)
+        self.questions = [questions[i] for i in train_indices]
+        self.labels = [labels[i] for i in train_indices]
+        self.map = map
+
+
+    def __len__(self):
+        return len(self.questions)
+
+
+    def __getitem__(self, idx):
+        return self.questions[idx], self.labels[idx]
+    
+
+    def __getMap__(self, idx):
+        return self.map[idx]
